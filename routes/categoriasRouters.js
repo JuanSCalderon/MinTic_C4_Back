225a55c4df0a81b7255cc7e9db561e477260507f
JS <juancalderonh@gmail.com>
2022-12-15 19:26:00 -0500

    Clase 21: Añadiendo Docker Containers, files y git ignore

--- routes/categoriasRouters.js
+++ routes/categoriasRouters.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const authMidd = require("../middleware/authMidd");
 const categoriasController = require("../controllers/categoriasController");
 
-router.get("/", categoriasController.obtenerCategoriaHome );
-router.get("/", authMidd, categoriasController.obtenerCategoria   );
-router.get("/:id", authMidd, categoriasController.obtenerCategoriaId   );
-router.post("/", authMidd,categoriasController.crearCategoria );
+router.get("/home", categoriasController.obtenerCategoriaHome);
+router.get("/", authMidd, categoriasController.obtenerCategoria);
+router.get("/:id", authMidd, categoriasController.obtenerCategoriaId);
+router.post("/", authMidd, categoriasController.crearCategoria);
 router.put("/:id", authMidd, categoriasController.actualizarCategoria);
-router.delete("/:id",authMidd,categoriasController.borrarCategoria );
+router.delete("/:id", authMidd, categoriasController.borrarCategoria);
 
 // definir las rutas
 module.exports = router;
