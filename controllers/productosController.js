b85ea114579eabcb91ab2f006707b5dfe89c8f62
JS <juancalderonh@gmail.com>
2022-12-20 15:56:16 -0500

    Cambios Front

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,37 +1,42 @@
 const Productos = require("../models/Productos");
 
-exports.obtenerProductosHome = async ( req, res) => {
-    try{
+exports.obtenerProductosHome = async (req, res) => {
+    try {
         const productos = await Productos.find();
 
         res.json({ productos });
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 
 };
 
-exports.obtenerProducto = async ( req, res) => {
+exports.obtenerProducto = async (req, res) => {
     const { id } = req.params
     const producto = await Productos.find().where("categoriaId").equals(id);
     res.json(producto);
 
 };
-exports.crearProducto = async ( req, res) => {
-    try{
+exports.crearProducto = async (req, res) => {
+    try {
         const producto = new Productos(req.body);
 
         producto.save();
         res.json(producto);
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 };
 
-exports.actualizarProducto = async ( req, res) => {
-    res.status(404).json({ msg : "actualizar producto"});
+exports.actualizarProducto = async (req, res) => {
+    res.status(404).json({ msg: "actualizar producto" });
 };
 
-exports.borrarProducto = async ( req, res) => {
-    res.status(404).json({ msg : "borrar producto"});
+exports.borrarProducto = async (req, res) => {
+    try {
+        await Productos.deleteOne({ _id: req.params.id });
+        res.json({ msg: "Product Deleted" });
+    } catch (error) {
+        console.log(error);
+    }
 };
--- models/Productos.js
+++ models/Productos.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const ProductosSchema = mongoose.Schema ({
-    nombre:{ type: String, required : true , trim: true },
-    descripcion:{ type: String, required : true , trim: true},
-    stock: { type: Number, required : true, trim: true },
-    precio:{ type: Number, required : true, trim: true },
-    imagen: { type: String, required : true , trim: true },
-    creado: { type : Date, default: Date.now()},
+const ProductosSchema = mongoose.Schema({
+    nombre: { type: String, required: true, trim: true },
+    descripcion: { type: String, required: true, trim: true },
+    stock: { type: Number, required: true, trim: true },
+    precio: { type: Number, required: true, trim: true },
+    imagen: { type: String, required: true, trim: true },
+    creado: { type: Date, default: Date.now() },
     categoriaId: { type: mongoose.Schema.Types.ObjectId, ref: "Categorias" }
 });
 // definir el modelo
